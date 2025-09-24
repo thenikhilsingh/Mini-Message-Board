1be1718eb2fdee3a9e2ff135a46f97957b5f82b7
thenikhilsingh <149455116+thenikhilsingh@users.noreply.github.com>
2025-09-24 15:49:42 +0530

    added new messages

--- db.js
+++ db.js
@@ -1,6 +1,22 @@
 const messages = [
-  { id: 1, text: "Hi there!", user: "Amando", added: new Date() },
-  { id: 2, text: "Hello World!", user: "Charles", added: new Date() },
+  {
+    id: 1,
+    text: "Who brought the snacks for this message board?",
+    user: "Nikhil",
+    added: new Date(),
+  },
+  {
+    id: 2,
+    text: "I thought you said virtual snacks count",
+    user: "Hriday",
+    added: new Date(),
+  },
+  {
+    id: 3,
+    text: "Then I’m eating them all IRL",
+    user: "Abhay",
+    added: new Date(),
+  },
 ];
 
 module.exports = messages;
